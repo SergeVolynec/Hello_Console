8af1050ae65b2dcf82c8c726ed14885725040df0
Serge Volynets <sergej-volynec@yandex.ru>
2023-02-09 16:59:56 +0300

    Example005_Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Да это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine (username);
+}
