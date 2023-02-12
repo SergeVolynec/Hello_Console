65b857ce8d299767f024a19c84bd58f939acdb07
Serge Volynets <sergej-volynec@yandex.ru>
2023-02-12 14:53:23 +0300

    Lecture2

---
+++ Example009_MethodArraySearch/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 43, 69, 28, 19, 69, 66, 11, 24 };
+int find = 69;
+int n = array.Length;
+int index = 0;
+
+while (index < n)
+{
+    if (find == array[index])
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] collection)
+{
+    int n = collection.Length;
+    int index = 0;
+    while (index < n)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf (int[] collection, int find)
+{
+    int size = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index<size)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    } 
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf (array, 44);
+Console.WriteLine(pos);
